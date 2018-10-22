19b550d4aacdeb7129bd8d2ca6ac89ef20037e61
carlos sepúlveda <carlos.sepulveda.a@usach.cl>
2018-10-22 19:38:02 -0300

    complement Table

---
+++ src/component/table/Table.js
@@ -0,0 +1,55 @@
+import React,{Component} from "react";
+import "./Table.css";
+
+/*
+function showDate(date){
+  var otraDate = new Date(date + 86400000);
+  console.log(otraDate);
+  console.log(otraDate.getMonth());
+  var fecha = otraDate.getDate() + '/' + (otraDate.getMonth()+1) +'/' + otraDate.getFullYear();
+  return fecha;
+}
+
+function ListaItems(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <tr>
+      <td>{number.productCode}</td>
+      <td>{number.productName}</td>
+      <td>{showDate(number.expirationDate)}</td>
+      <td>{number.category}</td>
+      <td>{number.price}</td>
+    </tr>
+
+  );
+  return (
+    <tbody>{listItems}</tbody>
+  );
+}
+*/
+
+class Table extends Component {  
+
+  render() {
+    return (   
+       <div class="panel">
+            <h3>
+             Lista de Enunciados
+            </h3>            
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Código </th>
+                  <th>Nombre  </th>
+                  <th type>Fecha de Publicación</th>
+                  <th>Descripción</th>
+                  <th>Fecha de término</th>
+                </tr>
+              </thead>
+            </table>
+        </div>     
+    );
+  }
+}
+
+export default Table;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import Login from './component/login/Login.js';
 import Register from './component/register/Register.js';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import Tabla from './component/tabla/Tabla.js';
+import Table from './component/table/Table.js';
 
-ReactDOM.render(<Tabla/>, document.getElementById('root'));
+ReactDOM.render(<Table/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
