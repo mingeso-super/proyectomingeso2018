e1b8d496181d6844df6d6f0ac2725a656c013da9
carlos sepúlveda <carlos.sepulveda.a@usach.cl>
2018-10-22 15:20:37 -0300

    component Register

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './component/App';
+import App from './component/App';
 import * as serviceWorker from './serviceWorker';
 import Login from './component/login/Login.js';
+import Register from './component/register/Register.js';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Register />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
